990d3a12a6f23ef58f0bd1e3cea79bea95a8f626
Carl Dominise <janncarlmykodominise@gmail.com>
2026-04-13 16:15:35 +0800

    Version 0.3 | Finished calculations functionality + organized into sorted table

--- console.py
+++ console.py
@@ -1,48 +1,57 @@
-import numpy as np
-
 def FCFS(burstTime, arrivalTime):
 
     n = len(burstTime)
+
+    process = []
     finishTime = []
     turnaroundTime = []
     waitingTime = []
-
+    table = []
+ 
     AveTAT = 0
     AveWT = 0
     currentTime = 0
     checker = 0
+    pCounter = 0
+
+    for i in range(n):
+        pCounter += 1
+        process.append(pCounter)
 
     for i in arrivalTime:
         checker += i
 
+    print(checker)
+
     if checker > 0:
-        processes = sorted(zip(arrivalTime, burstTime))
+        processes = sorted(zip(arrivalTime, burstTime, process))
 
         arrivalTime = [p[0] for p in processes]
         burstTime = [p[1] for p in processes]
+        process = [p[2] for p in processes] 
 
 
     # Finish Time calculation
     for i in range(n):
 
         if currentTime < arrivalTime[i]:
-            currentTime = arrivalTime[i]
+           currentTime = arrivalTime[i]
 
         currentTime += burstTime[i]
         finishTime.append(currentTime)
-    print(finishTime)
+    #print(finishTime)
 
     # Turnaround Time Calculation
     for i in range(n):
 
         turnaroundTime.append(finishTime[i] - arrivalTime[i])
-    print(turnaroundTime)
+    #print(turnaroundTime)
 
     # Waiting Time Calculation
     for i in range(n):
 
         waitingTime.append(turnaroundTime[i] - burstTime[i])
-    print(waitingTime)
+    #print(waitingTime)
 
     #Average Calculations
     sumTAT = 0
@@ -55,15 +64,28 @@ def FCFS(burstTime, arrivalTime):
     AveTAT = float(sumTAT)/n
     AveWT = float(sumWT)/n
 
+
+    # Combining everything into a table
+    for i in range(n):
+        temp = []
+
+        temp.append(process[i])
+        temp.append(arrivalTime[i])
+        temp.append(burstTime[i])
+        temp.append(finishTime[i])
+        temp.append(turnaroundTime[i])
+        temp.append(waitingTime[i])
+
+        table.append(temp)
+
+    sorted_table = sorted(table)
+
+    for i in sorted_table:
+        print(i)
+
     print(f"Average TAT = {AveTAT}" )
     print(f"Average WT = {AveWT}")
         
-
-    # Combining everything into a table
-
-
-
-
 
 #---------------------------------------------------------------------------------------------------------------
 
@@ -87,9 +109,4 @@ for a in arrivalTime:
 
 FCFS(BTime, ATime)
 #SJF(BTime, ATime)
-
-
-
-
-
 
